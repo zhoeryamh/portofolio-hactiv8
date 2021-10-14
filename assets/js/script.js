837f9c11914b2c5295e93488fb07d9e4c9ebb1e0
unknown <suryamaulana662@gmail.com>
2021-10-14 13:45:22 +0700

    Added Auto Scroll on Navbar (14/10)

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,16 @@ $(window).on("scroll", () => {
     $body.removeClass(scrollClass);
   }
 });
+
+
+$(".nav-link").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 500, function(){
+      window.location.hash = hash;
+    });
+  }
+});
