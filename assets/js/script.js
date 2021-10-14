07cfa9cda048e1c3a7e5d373711fb806716bd875
unknown <suryamaulana662@gmail.com>
2021-10-14 15:32:19 +0700

    Fixing CSS Overflow (14/10)

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,14 @@ $(".nav-link").on('click', function(event) {
     });
   }
 });
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
